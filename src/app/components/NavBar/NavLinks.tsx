ab6213de493aa9923876d745b64491a3482cb2d5
Gramloul-Zineeddine <129644181+Gramloul-Zineeddine@users.noreply.github.com>
2025-03-14 14:22:12 +0100

    modéfication nav bar

--- src/app/components/NavBar/NavLinks.tsx
+++ src/app/components/NavBar/NavLinks.tsx
@@ -1,49 +1,39 @@
 "use client";
 
-import clsx from 'clsx';
-import {User,BriefcaseBusiness  } from 'lucide-react';
-import Link from 'next/link';
+import clsx from "clsx";
+import { User, BriefcaseBusiness } from "lucide-react";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-
 const links = [
-    { name: 'Client', href: '/Client', icon: User },
-    
-    { name: 'Travaux', href: '/Travaux', icon: BriefcaseBusiness   },
-    
-
+  { name: "Client", href: "/Client", icon: User },
+  { name: "Travaux", href: "/Travaux", icon: BriefcaseBusiness },
 ];
 
 export default function NavLinks() {
-    const pathname = usePathname();
-    // set default to dashboard
-    let linkColored =0;
+  const pathname = usePathname();
 
-    links.map((link,index) => {
-        if (pathname.startsWith(link.href)) {
-            linkColored =index;
-        }
-    });
-    return (
-        <>
-            {links.map((link,index) => {
+  return (
+    <>
+      {links.map((link) => {
+        const isActive = pathname.startsWith(link.href);
 
-                return (
-                    <Link
-                        key={link.name}
-                        href={link.href}
-                        className={clsx(
-                            "flex h-[48px] grow items-center justify-center gap-4 rounded-md bg-white p-3 text-sm font-medium hover:bg-blue-500 hover:text-white md:flex-none md:justify-start md:p-2 md:px-3",
-                            {
-                                'text-white bg-blue-500 hover:bg-blue-300': index===linkColored,
-                            }
-                        )}
-                    >
-                        <link.icon size={20} />
-                        <p className="hidden md:block">{link.name}</p>
-                    </Link>
-                );
-            })}
-        </>
-    );
+        return (
+          <Link
+            key={link.name}
+            href={link.href}
+            className={clsx(
+              "flex h-[48px] grow items-center justify-center gap-4 rounded-md bg-white p-3 text-sm font-medium hover:bg-blue-400 hover:text-black md:flex-none md:justify-start md:p-2 md:px-3",
+              {
+                "bg-blue-400 text-black": isActive, // Changement ici
+              }
+            )}
+          >
+            <link.icon size={20} />
+            <p className="hidden md:block">{link.name}</p>
+          </Link>
+        );
+      })}
+    </>
+  );
 }
--- src/app/components/NavBar/SideBar.tsx
+++ src/app/components/NavBar/SideBar.tsx
@@ -4,11 +4,12 @@ import NavLinks from './NavLinks';
 
 const SideBar = () => {
   return (
-    <div className='h-screen md:w-56 bg-white  p-3'>
+    <div className='h-screen md:w-64 bg-white  p-3'>
       {/* Logo */}
       <div className="flex justify-center">
         <Image src="/Google.png" alt="Logo OBA COLOR" width={100} height={100} />
       </div>
+
 
       {/* Navigation Links */}
       <div className='flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2 mt-5'>
